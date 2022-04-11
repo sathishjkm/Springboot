f63b65a4b52a33ed53e4c3e794e80c34bf832920
sathis <latchulatchu02@gmail.com>
2022-04-11 17:30:54 +0530

    Springboot(teacher)

---
+++ src/main/java/socialmedia/socialmedia/Teacher.java
@@ -0,0 +1,30 @@
+package socialmedia.socialmedia;
+
+public class Teacher {
+    private String id;
+    private String name;
+    private String subject;
+    public Teacher(String id, String name, String subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
